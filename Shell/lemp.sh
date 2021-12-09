ed705def7eaeffc762cf25d99406f1b7b602ee39
nagulvali555 <nagulvali555@gmail.com>
2021-12-09 13:10:42 +0530

    fixes and debian support

--- Shell/lemp.sh
+++ Shell/lemp.sh
@@ -6,14 +6,22 @@
 # 2. centos
 # Script version: 1.0.0
 
-# Issues: version dependencies
+header() {
+  cat <<EOF
+"###################################################"
+"# Program: LEMP Installation Script               #"
+"# Description: Installs and setup LEMP server     #"
+"#              nginx, mariadb, php fast-cgi       #"
+"# Supported Platforms: Ubuntu, CentOS             #"
+"###################################################"
+EOF
+}
 
 # log
 logger() {
   level=$(echo "$1" | tr '[a-z]' '[A-Z]')
   message=$2
   echo "[$(date)]::[$level]:: $message"
-  exit 1
 }
 
 # root user access
@@ -27,37 +35,60 @@ elevated_user_check() {
 centos() {
   # update repos and install lemp stack
   yum update -y \
-  && yum install epel-release \
-  && yum install nginx \
+  && yum install -y epel-release \
+  && yum install -y nginx \
   && systemctl start nginx \
   && systemctl enable nginx \
-  && yum install mariadb-server mariadb \
-  && systemctl enable mariadb \
-  && yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm \
+  && yum install -y mariadb-server mariadb \
+  && systemctl -y enable mariadb \
+  && yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm \
   && yum --disablerepo="*" --enablerepo="remi-safe" list php[7-9][0-9].x86_64 \
   && yum-config-manager --enable remi-php74 \
-  && yum install php php-mysqlnd php-fpm \
+  && yum install -y php php-mysqlnd php-fpm \
   && logger 'info' 'Installations successful'
+
+  logger 'info' 'Updating config files'
+  cp -rf ../config/www.conf /etc/php-fpm.d/www.conf \
+  && cp -rf ../config/nginx.conf /etc/nginx/nginx.conf \
+  && systemctl start php-fpm \
+  && systemctl restart nginx
+
+  logger 'info' 'To setup database execute \"mysql_secure_installation\" command'
 }
 
 debian () {
-  echo
+
+  # update repos and install lemp stack
+  apt update \
+  && apt install nginx \
+  && apt install mysql-server \
+  && apt install php-fpm php-mysql \
+  && systemctl enable nginx \
+  && systemctl start nginx \
+  && systemctl start mysqld \
+  && systemctl enable mysqld
+
 }
 
 # Check platform
 main() {
+
+  # welcome message
+  header
+
   # check elevated user
   elevated_user_check
 
   # Installation based on linux distribution
-  case $(cat /etc/os-release | grep -i "^name" | cut -d "\"" -f 2) in
+  case $(grep -i "^name" /etc/os-release | cut -d "\"" -f 2) in
   "Ubuntu")
     debian;;
-  "Centos"|"Amazon Linux")
+  "CentOS Linux"|"Amazon Linux")
     centos;;
   *)
     logger 'error' 'Linux distribution is not compatible';;
   esac
+
 }
 
 # main
